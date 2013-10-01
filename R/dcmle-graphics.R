f985a1374fa3bb25f3ea16b8f84773bca68923e2
Peter Solymos <solymos@ualberta.ca>
2013-10-01 23:10:49 +0000

    version 0.2-4

--- R/dcmle-graphics.R
+++ R/dcmle-graphics.R
@@ -1,15 +1,15 @@
 ## plot methods (coda and lattice)
 
 setMethod("plot", signature(x="MCMClist", y="missing"), function(x, ...) 
-    coda:::plot.mcmc.list(x, ...))
+    plot(as(x, "mcmc.list"), ...))
 setMethod("plot", signature(x="codaMCMC", y="missing"), function(x, ...) 
-    coda:::plot.mcmc.list(as(x, "MCMClist"), ...))
+    plot(as(x, "MCMClist"), ...))
 setMethod("plot", signature(x="dcmle", y="missing"), function(x, ...) 
-    coda:::plot.mcmc.list(as(x, "MCMClist"), ...))
+    plot(as(x, "MCMClist"), ...))
 
 #setGeneric("traceplot", function(x, ...) standardGeneric("traceplot"))
 setMethod("traceplot", "MCMClist", function(x, ...) 
-    coda:::traceplot(x, ...))
+    coda::traceplot(x, ...))
 setMethod("traceplot", "codaMCMC", function(x, ...) 
     traceplot(as(x, "MCMClist"), ...))
 setMethod("traceplot", "dcmle", function(x, ...) 
@@ -17,7 +17,7 @@ setMethod("traceplot", "dcmle", function(x, ...)
 
 #setGeneric("densplot", function(x, ...) standardGeneric("densplot"))
 setMethod("densplot", "MCMClist", function(x, ...) 
-    coda:::densplot(x, ...))
+    coda::densplot(x, ...))
 setMethod("densplot", "codaMCMC", function(x, ...) 
     densplot(as(x, "MCMClist"), ...))
 setMethod("densplot", "dcmle", function(x, ...) 
@@ -25,7 +25,7 @@ setMethod("densplot", "dcmle", function(x, ...)
 
 #setGeneric("pairs", function(x, ...) standardGeneric("pairs"))
 setMethod("pairs", "MCMClist", function(x, ...) 
-    dclone:::pairs.mcmc.list(x, ...))
+    dclone::pairs.mcmc.list(x, ...))
 setMethod("pairs", "codaMCMC", function(x, ...) 
     pairs(as(x, "MCMClist")))
 setMethod("pairs", "dcmle", function(x, ...) 
@@ -33,46 +33,46 @@ setMethod("pairs", "dcmle", function(x, ...)
 
 #setGeneric("densityplot", function(x, ...) standardGeneric("densityplot"))
 setMethod("densityplot", "MCMClist", 
-    function(x, ...) 
-        coda:::densityplot.mcmc.list(x, ...))
+    function(x, data, ...) 
+        densityplot(as(x, "mcmc.list"), data, ...))
 setMethod("densityplot", "codaMCMC", 
-    function(x, ...) 
+    function(x, data, ...) 
         densityplot(as(x, "MCMClist"), data, ...))
 setMethod("densityplot", "dcmle", 
-    function(x, ...) 
+    function(x, data, ...) 
         densityplot(as(x, "MCMClist"), data, ...))
 
 #setGeneric("qqmath", function(x, ...) standardGeneric("qqmath"))
 setMethod("qqmath", "MCMClist", 
-    function(x, ...) 
-        coda:::qqmath.mcmc.list(x, ...))
+    function(x, data, ...) 
+        qqmath(as(x, "mcmc.list"), data, ...))
 setMethod("qqmath", "codaMCMC", 
-    function(x, ...) 
+    function(x, data, ...) 
         qqmath(as(x, "MCMClist"), data, ...))
 setMethod("qqmath", "dcmle", 
-    function(x, ...) 
+    function(x, data, ...) 
         qqmath(as(x, "MCMClist"), data, ...))
 
 #setGeneric("xyplot", function(x, ...) standardGeneric("xyplot"))
 setMethod("xyplot", "MCMClist", 
-    function(x, ...) 
-        coda:::xyplot.mcmc.list(x, ...))
+    function(x, data, ...) 
+        xyplot(as(x, "mcmc.list"), data, ...))
 setMethod("xyplot", "codaMCMC", 
-    function(x, ...) 
+    function(x, data, ...) 
         xyplot(as(x, "MCMClist"), data, ...))
 setMethod("xyplot", "dcmle", 
-    function(x, ...) 
+    function(x, data, ...) 
         xyplot(as(x, "MCMClist"), data, ...))
 
 #setGeneric("acfplot", function(x, ...) standardGeneric("acfplot"))
 setMethod("acfplot", "MCMClist", 
-    function(x, ...) 
-        coda:::acfplot.mcmc.list(x, ...))
+    function(x, data, ...) 
+        acfplot(as(x, "mcmc.list"), data, ...))
 setMethod("acfplot", "codaMCMC", 
-    function(x, ...) 
+    function(x, data, ...) 
         acfplot(as(x, "MCMClist"), data, ...))
 setMethod("acfplot", "dcmle", 
-    function(x, ...) 
+    function(x, data, ...) 
         acfplot(as(x, "MCMClist"), data, ...))
 
 ## this plots only mcmc (one chain at a time)
@@ -91,4 +91,13 @@ setMethod("crosscorr.plot", "dcmle", function(x, ...)
 setMethod("crosscorr.plot", "codaMCMC", function(x, ...) 
     crosscorr.plot(as(x, "MCMClist"), ...))
 setMethod("crosscorr.plot", "MCMClist", function(x, ...) 
-    coda:::crosscorr.plot(x, ...))
+    coda::crosscorr.plot(x, ...))
+
+setGeneric("gelman.plot", function(x, ...) 
+    standardGeneric("gelman.plot"))
+setMethod("gelman.plot", "dcmle", function(x, ...) 
+    gelman.plot(as(x, "MCMClist"), ...))
+setMethod("gelman.plot", "codaMCMC", function(x, ...) 
+    gelman.plot(as(x, "MCMClist"), ...))
+setMethod("gelman.plot", "MCMClist", function(x, ...) 
+    coda::gelman.plot(x, ...))
